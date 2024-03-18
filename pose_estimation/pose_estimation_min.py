22dfb6c15366c5e03911b3601798ea2496fd9b8c
James <jamesyuan1000@gmail.com>
2024-03-19 00:05:06 +0100

    created bare minimum code for pose tracking

--- pose_estimation/pose_estimation_min.py
+++ pose_estimation/pose_estimation_min.py
@@ -2,28 +2,56 @@ import cv2
 import mediapipe as mp
 import time
 
+mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
+# Load video into cap
 video_path = "/mnt/c/Users/james/OneDrive/Desktop/Computer vision videos/PoseVideos/"
-cap = cv2.VideoCapture(video_path + "video2.mp4")
+cap = cv2.VideoCapture(video_path + "video4.mp4")
 
 prev_time = 0
 
 
 while True:
     success, img = cap.read()
-    cv2.imshow("Random video lol", img)
+
+    # Read colour in RGB - image convert from BGR --> RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Processing and displaying pose landmark locations
+    result = pose.process(imgRGB)
+    print(result.pose_landmarks)
+
+    # Draw pose lines
+    if result.pose_landmarks:
+        mpDraw.draw_landmarks(img, result.pose_landmarks, mpPose.POSE_CONNECTIONS)
+
+        # For each set of pose landmarks, print landmark information (x, y)
+        for id, lm in enumerate(result.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id, lm)
+
+            # Convert image ratio value to pixel value
+            x_pixel_value = int(lm.x * w)
+            y_pixel_value = int(lm.y * h)
+
+            # Overlay our circle onto detected landmarks
+            cv2.circle(img, (x_pixel_value, y_pixel_value), 10, (0, 100, 255), cv2.FILLED)
+
 
 
     # Display FPS
     curr_time = time.time()
-    fps = 1/curr_time - prev_time
+    fps = 1/ (curr_time - prev_time)
     prev_time = curr_time
-    cv2.putText(img, "FPS counter: " + str(int(fps)), (70, 50), cv2.FONT_HERSHEY_SCRIPT_COMPLEX, 3, (0, 0, 255), 3)
+    cv2.putText(img, "FPS Counter: " +str(int(fps)), (70, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 1)
+
+    # Display video with our modifications
+    cv2.imshow("Random video lol", img)
 
 
-
+    # Waits for 1 milisecond after video finishes playing before destroying video window
     cv2.waitKey(1)
 
     
